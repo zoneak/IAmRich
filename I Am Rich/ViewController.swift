f925eb159fa0cba821571535fbab1c15c6791bcd
Adriano <adriano.kaito@gmail.com>
2019-09-19 21:15:21 -0300

    finished

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Adriano Kaito on 17/09/19.
+//  Copyright © 2019 AK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
